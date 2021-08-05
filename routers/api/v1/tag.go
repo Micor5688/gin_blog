e2ae62f20677089134e7b05f7fbf0269cb446389
micor5688 <micor5688@163.com>
2021-08-05 14:22:32 +0800

    编写标签列表的路由逻辑

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,9 +1,40 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin_blog/models"
+	"gin_blog/pkg/e"
+	"gin_blog/pkg/setting"
+	"gin_blog/pkg/util"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+)
 
 //获取多个文章标签
 func GetTags(c *gin.Context) {
+	name := c.Query("name")
+
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+
+	if name != "" {
+		maps["name"] = name
+	}
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+	code := e.SUCCESS
+	data["list"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["tatal"] = models.GetTagTotal(maps)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 //新增文章标签
